65235bb916260b426a7938d832f614b33d9a5b2b
saul890 <102773536+saul890@users.noreply.github.com>
2022-11-02 15:14:23 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const a = document.getElementById("a")
+const b = document.getElementById("b")
+const c = document.getElementById("c")
+const d = document.getElementById("d")
+const e = document.getElementById("e")
+const f = document.getElementById("f")
+const g = document.getElementById("g")
+const h = document.getElementById("h")
+const i = document.getElementById("i")
+const j = document.getElementById("j")
+const k = document.getElementById("k")
+const l = document.getElementById("l")
+const m = document.getElementById("m")
+const n = document.getElementById("n")
+const o = document.getElementById("o")
+const p = document.getElementById("p")
+const q = document.getElementById("q")
+const r = document.getElementById("r")
+const s = document.getElementById("s")
+const t = document.getElementById("t")
+const u = document.getElementById("u")
+const v = document.getElementById("v")
+const w = document.getElementById("w")
+const x = document.getElementById("x")
+const y = document.getElementById("y")
+const z = document.getElementById("z")
+
+a.addEventListener('mouseenter', () => a.style.opacity = "1"); 
+b.addEventListener('mouseenter', () => b.style.opacity = "1"); 
+c.addEventListener('mouseenter', () => c.style.opacity = "1"); 
+d.addEventListener('mouseenter', () => d.style.opacity = "1"); 
+e.addEventListener('mouseenter', () => e.style.opacity = "1"); 
+f.addEventListener('mouseenter', () => f.style.opacity = "1"); 
+g.addEventListener('mouseenter', () => g.style.opacity = "1"); 
+h.addEventListener('mouseenter', () => h.style.opacity = "1"); 
+i.addEventListener('mouseenter', () => i.style.opacity = "1"); 
+j.addEventListener('mouseenter', () => j.style.opacity = "1"); 
+k.addEventListener('mouseenter', () => k.style.opacity = "1"); 
+l.addEventListener('mouseenter', () => l.style.opacity = "1"); 
+m.addEventListener('mouseenter', () => m.style.opacity = "1"); 
+n.addEventListener('mouseenter', () => n.style.opacity = "1"); 
+o.addEventListener('mouseenter', () => o.style.opacity = "1"); 
+p.addEventListener('mouseenter', () => p.style.opacity = "1"); 
+q.addEventListener('mouseenter', () => q.style.opacity = "1"); 
+r.addEventListener('mouseenter', () => r.style.opacity = "1"); 
+s.addEventListener('mouseenter', () => s.style.opacity = "1"); 
+t.addEventListener('mouseenter', () => t.style.opacity = "1"); 
+u.addEventListener('mouseenter', () => u.style.opacity = "1"); 
+v.addEventListener('mouseenter', () => v.style.opacity = "1"); 
+w.addEventListener('mouseenter', () => w.style.opacity = "1"); 
+x.addEventListener('mouseenter', () => x.style.opacity = "1"); 
+y.addEventListener('mouseenter', () => y.style.opacity = "1"); 
+z.addEventListener('mouseenter', () => z.style.opacity = "1"); 
+
